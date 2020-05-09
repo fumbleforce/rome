11a2949b8f092dad1b4300f4944fb4b388123991
jorgeer <jfemail3@gmail.com>
2020-05-09 16:35:37 +0200

    add comment to findOne transform

--- src/server/collections.js
+++ src/server/collections.js
@@ -1,6 +1,8 @@
 import { collection } from "hidden_mongo";
 
 export const User = collection("users", {
+  // Remove password hash from normal queries to avoid accidentally showing it.
+  // Full user can still be retrieved using _findOne.
   transformFindOne ({ passwordHash, ...result }) {
     return result;
   }
