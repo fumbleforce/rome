d8f083aabb92f13dcb6ec717c626285f2e3633e5
jorgeer <jfemail3@gmail.com>
2020-05-09 12:35:02 +0200

    remove watch

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,2 @@
 import "./game_methods";
 import "./user_methods";
-
-import Hidden from "hidden_server";
-import { User } from "./collections";
-
-Hidden.startup(() => {
-  User.watch().on("change", (...args) => {
-    console.log("user change", args);
-  });
-});
